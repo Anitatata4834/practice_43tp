63d79158382df9b9270a927733a0fd7194d6084c
Anitatata4834 <Anna82rosepkina@gmail.com>
2026-04-01 20:03:02 +0300

    Сделала 14 главу

---
+++ day14/Task1/EditEmployee.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Task1
+{
+    /// <summary>
+    /// Логика взаимодействия для EditEmployee.xaml
+    /// </summary>
+    public partial class EditEmployee : Window
+    {
+        public string FullName => FullNameBox.Text;
+        public string Position => PositionBox.Text;
+        public EditEmployee(Employee selected)
+        {
+            InitializeComponent();
+            DataContext = selected;
+            FullNameBox.Text = selected.FullName;
+            PositionBox.Text = selected.Position;
+        }
+        private void EditEmployee_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+    }
+}
