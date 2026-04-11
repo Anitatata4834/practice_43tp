fff5a5848e91681566339b70deafac0c172b4d67
Anitatata4834 <Anna82rosepkina@gmail.com>
2026-04-11 09:08:42 +0300

    Сделала 19 главу

---
+++ day19/WatchList/Controllers/WatchController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WatchList.Models;
+
+namespace WatchList.Controllers
+{
+    public class WatchController : Controller
+    {
+        private static List<WatchItem> _items = new List<WatchItem>();
+        private static int _nextId = 1;
+
+        public IActionResult Index()
+        {
+            return View(_items);
+        }
+
+        [HttpPost]
+        public IActionResult Add(string title, string type, string status)
+        {
+            if (!string.IsNullOrWhiteSpace(title))
+            {
+                WatchItem item = new WatchItem();
+                item.Id = _nextId;
+                item.Title = title;
+                item.Type = type;
+                item.Status = status;
+
+                _items.Add(item);
+                _nextId = _nextId + 1;
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult MarkAsWatched(int id)
+        {
+            WatchItem item = _items.FirstOrDefault(x => x.Id == id);
+            if (item != null)
+            {
+                item.Status = "Просмотрено";
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
